3389759a298fc2366889c624a5bc760f0a71b53f
sripriyesha <68758690+sripriyesha@users.noreply.github.com>
2021-07-05 10:15:57 +0200

    update 31 to 147

--- launch_scraper.sh
+++ launch_scraper.sh
@@ -1,7 +1,7 @@
 # launch script with page number as first argument
 
-# for i in {16..383}
-# do
-    # python3 fetch_links_with_selenium.py "$i"
-    scrapy crawl books_metadata -s JOBDIR=crawls/books_metadata_page_"$1" -o metadata/metadata_page_"$1".csv -a page="$1"
-# done
+for i in {148..383}
+do
+    python3 fetch_links_with_selenium.py "$i"
+    scrapy crawl books_metadata -s JOBDIR=crawls/books_metadata_page_"$i" -o metadata/metadata_page_"$i".csv -a page="$i"
+done
